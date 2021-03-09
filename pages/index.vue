c42e2fab8f28785e353e5aec6e04cbbd7a917f66
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-09 23:38:03 +0100

    :sparkles: (base) update base components

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,9 @@
     <div>
       <Logo />
       <h1 class="title">rapide</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
 
 <style>
 .container {
@@ -45,21 +21,10 @@ export default Vue.extend({})
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
-  font-weight: 300;
+  font-weight: 600;
   font-size: 100px;
   color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  letter-spacing: 4px;
+  font-style: italic;
 }
 </style>
