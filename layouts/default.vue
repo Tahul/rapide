45cf3d1c520d53a4fbcadd681d3c1175e22abe87
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-19 22:08:06 +0200

    :wrench: (nuxt.config) document nuxt.config.ts

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,25 +4,19 @@
 
 <style>
 html,
-body {
+body,
+#__layout,
+#__nuxt {
   height: 100%;
-  min-height: 100%;
 }
 
 html {
-  font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
+  font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI';
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
+  font-size: 16px;
   box-sizing: border-box;
-}
-
-#__layout,
-#__nuxt {
-  height: 100%;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,52 @@
 export default {
+  /**
+   * Docs: https://fr.nuxtjs.org/docs/2.x/configuration-glossary/configuration-target
+   */
   target: 'static',
 
+  /**
+   * Docs: https://fr.nuxtjs.org/docs/2.x/configuration-glossary/configuration-head
+   */
   head: {
-    title: 'rapide',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    // App default title
+    title: 'Rapide 🏎',
+    // Page meta tags
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap'
       }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    ]
   },
 
-  css: [],
-
-  plugins: [],
-
+  /**
+   * Docs: https://github.com/nuxt/components
+   */
   components: true,
 
-  buildModules: ['nuxt-vite', '@nuxt/typescript-build', 'nuxt-windicss'],
+  /**
+   * Docs: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-css
+   */
+  css: [],
 
-  modules: []
+  /**
+   * Docs: https://fr.nuxtjs.org/docs/2.x/configuration-glossary/configuration-plugins
+   */
+  plugins: [],
+
+  /**
+   * Docs: https://fr.nuxtjs.org/docs/2.x/configuration-glossary/configuration-modules
+   */
+  modules: [],
+
+  /**
+   * Docs: https://fr.nuxtjs.org/docs/2.x/configuration-glossary/configuration-modules#buildmodules
+   */
+  buildModules: ['nuxt-vite', '@nuxt/typescript-build', 'nuxt-windicss']
 }
