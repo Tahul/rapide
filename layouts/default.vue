ad636dc1ea20dcf7c49765a64dec75d17cfee3a0
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-19 21:55:51 +0200

    :sparkles: (components) update page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,28 @@
 <template>
-  <Nuxt />
+  <Nuxt id="root" />
 </template>
+
+<style>
+html,
+body {
+  height: 100%;
+  min-height: 100%;
+}
+
+html {
+  font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+#__layout,
+#__nuxt {
+  height: 100%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex flex-col items-center justify-center w-full h-full">
     <Logo />
-    <h1 class="block mt-6 font-sans text-6xl italic font-bold text-red-400">
-      Rapide
-    </h1>
+    <h1 class="block mt-6 font-sans text-6xl font-bold text-red-400">Rapide</h1>
   </div>
 </template>
